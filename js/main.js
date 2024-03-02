b0ebef529452f6ff088c4e879d5b19fa0197c2c2
GomesZz <ggvgabriel05@gmail.com>
2024-03-01 22:52:21 -0300

    header Ok

---
+++ js/main.js
@@ -0,0 +1,40 @@
+function ChangeMenu() {
+    const groupMenu = document.getElementsByClassName('btn-group-vertical')[0];
+
+    groupMenu.style.flexDirection = 'row';
+    groupMenu.style.alignItems = 'center';
+    groupMenu.style.justifyContent = 'flex-start';
+    groupMenu.style.gap = '12px';
+    groupMenu.style.width = '100%';
+
+    const buttonsMenu = groupMenu.getElementsByClassName('btn-secondary');
+
+    for (let i = 0; i < buttonsMenu.length; i++) {
+        let item = buttonsMenu.item(i);
+        item.style.borderRadius = '5px';
+        item.style.width = 'fit-content';
+        item.style.flex = 'none';
+        item.style.padding = '0.375rem 1.2rem';
+    }
+}
+
+function ChangeHeader(){
+    const header = document.getElementsByClassName('jumbotron')[0];
+
+    header.style.backgroundColor = '#6c757d';
+    header.style.color = '#fff';
+    header.style.textAlign = 'right';
+
+    const buttonPrimary = document.getElementsByClassName('btn-primary')[0];
+
+    buttonPrimary.style.backgroundColor = '#27a844';
+    buttonPrimary.style.borderColor = '#27a844';
+    buttonPrimary.style.color = '#fff';
+}
+
+function init() {
+    // ChangeMenu();
+    // ChangeHeader();
+}
+
+init();
